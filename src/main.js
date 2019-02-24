6981b53b027ebe29b1218fc4552951ca48f89062
sazhnevea <sazhnevea@gmail.com>
2019-02-24 17:20:37 +0400

    listener added

--- src/main.js
+++ src/main.js
@@ -2,18 +2,31 @@ import makeFilter from './make-filter.js';
 import makePoint from './make-point.js';
 
 const filters = document.querySelector(`.trip-filter`);
+const pointsContainer = document.querySelector(`.trip-day__items`);
 
 filters.insertAdjacentHTML(`beforeend`, makeFilter(`everything`, `checked`));
 filters.insertAdjacentHTML(`beforeend`, makeFilter(`future`));
 filters.insertAdjacentHTML(`beforeend`, makeFilter(`past`));
 
-const pointsContainer = document.querySelector(`.trip-day__items`);
-
-const renderPoint = (dist) => {
-  const points = new Array(7)
+const renderPoints = (dist, count = 7) => {
+  const points = new Array(count)
     .fill()
     .map(makePoint);
   dist.insertAdjacentHTML(`beforeend`, points.join(``));
 };
 
-renderPoint(pointsContainer);
+renderPoints(pointsContainer);
+
+const getRandomNumber = (min = 1, max = 100) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+filters.addEventListener(`click`, (evt) => {
+  const isFilterTarget = evt
+                         .target
+                         .classList
+                         .contains(`trip-filter__item`);
+  if (isFilterTarget) {
+    pointsContainer.innerHTML = ``;
+    renderPoints(pointsContainer, getRandomNumber(1, 20));
+  }
+});
