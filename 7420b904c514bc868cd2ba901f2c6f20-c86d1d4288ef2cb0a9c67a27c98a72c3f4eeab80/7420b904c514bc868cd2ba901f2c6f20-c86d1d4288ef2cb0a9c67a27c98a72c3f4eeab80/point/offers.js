dac33f50b6975a41ec0a7f40a2d0ec873f70954b
Alex Masterov <alex.masterow@gmail.com>
2019-03-12 08:46:29 +0300

    Clean up

--- 7420b904c514bc868cd2ba901f2c6f20-c86d1d4288ef2cb0a9c67a27c98a72c3f4eeab80/7420b904c514bc868cd2ba901f2c6f20-c86d1d4288ef2cb0a9c67a27c98a72c3f4eeab80/point/offers.js
+++
@@ -1,9 +0,0 @@
-export const getOffers = (offers) => `
-  <ul class="trip-point__offers">
-  ${offers.map(({name, price}) => `
-    <li>
-      <button class="trip-point__offer">${name} +&euro;&nbsp;${price}
-      </button>
-    </li>`
-  ).join(``)}
-  </ul>`;
