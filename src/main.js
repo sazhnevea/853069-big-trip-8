1a6944f218bf13a0e32c8eb200ce82fc0bc82423
sazhnevea <sazhnevea@gmail.com>
2019-03-17 22:41:27 +0400

    10. Личный проект: расширяя горизонты

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import {getPoint} from './point';
+import Point from './point.js';
+import PointFull from './point-full.js';
 import {getFilter} from './filter';
-
 import {getPointData} from './mock/data';
 
 const filterNames = [
@@ -9,22 +9,43 @@ const filterNames = [
   `past`,
 ];
 
+const filterContainer = document.querySelector(`.trip-filter`);
+const pointsContainer = document.querySelector(`.trip-day__items`);
+
 const generateFilters = () => filterNames.map(getFilter).join(``);
-const generatePoints = (num = 3) => [...Array(num)]
-  .map(getPointData)
-  .map(getPoint)
-  .join(``);
 
-const filterForm = document.querySelector(`.trip-filter`);
-const dayItems = document.querySelector(`.trip-day__items`);
-
-const renderElements = (container, element) => {
-  container.innerHTML = element;
+const generatePoints = (count = 7) => {
+  return [...Array(count)].map(() => new Point(getPointData()));
 };
 
-filterForm.addEventListener(`change`, () => {
-  renderElements(dayItems, generatePoints());
+const renderFilters = (element) => {
+  filterContainer.innerHTML = element;
+};
+
+const fullPointComponent = new PointFull(getPointData());
+
+const renderPoints = (components) => {
+  components.forEach((pointComponent) => {
+    pointsContainer.appendChild(pointComponent.render());
+
+    pointComponent.onEdit = () => {
+      fullPointComponent.render();
+      pointsContainer.replaceChild(fullPointComponent.element, pointComponent.element);
+      pointComponent.unrender();
+    };
+
+    fullPointComponent.onSubmit = () => {
+      pointComponent.render();
+      pointsContainer.replaceChild(pointComponent.element, fullPointComponent.element);
+      fullPointComponent.unrender();
+    };
+  });
+};
+
+filterContainer.addEventListener(`change`, () => {
+  pointsContainer.innerHTML = ``;
+  renderPoints(generatePoints());
 });
 
-renderElements(dayItems, generatePoints());
-renderElements(filterForm, generateFilters());
+renderFilters(generateFilters());
+renderPoints(generatePoints());
