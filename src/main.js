600395b659c871863a2211d2b0731c2e1973b0de
sazhnevea <sazhnevea@gmail.com>
2019-04-08 11:24:20 +0400

    статистика не полностью

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Point from './point.js';
 import PointFull from './point-full.js';
+import Stats from './statistics.js';
 import Filter from './filter.js';
+
 import {getPointData} from './mock/data';
 import {filterNames} from './mock/filter-names';
 
@@ -74,3 +76,33 @@ filterContainer.addEventListener(`change`, ({target}) => {
 
 renderFilters(filterNames);
 renderPoints(pointsData);
+
+const mainContainer = document.getElementById(`table`);
+const statsContainer = document.getElementById(`stats`);
+const tableButton = document.querySelector(`.view-switch a:nth-child(1)`);
+const statsButton = document.querySelector(`.view-switch a:nth-child(2)`);
+
+statsButton.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  mainContainer.classList.add(`visually-hidden`);
+  statsContainer.classList.remove(`visually-hidden`);
+});
+
+tableButton.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+  mainContainer.classList.remove(`visually-hidden`);
+  statsContainer.classList.add(`visually-hidden`);
+});
+
+// const moneyCtx = document.querySelector(`.statistic__money`);
+const transportCtx = document.querySelector(`.statistic__transport`);
+// const timeSpentCtx = document.querySelector(`.statistic__time-spend`);
+
+// const money = new Stats(moneyCtx, pointsData, `money`);
+// money.render();
+
+const transport = new Stats(transportCtx, pointsData, `transport`);
+transport.render();
+
+// const timeSpent = new Stats(timeSpentCtx, pointsData, `time spent`);
+// timeSpent.render();
